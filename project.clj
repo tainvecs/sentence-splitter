bf135c9728b1cfdac0c0bd31e768492563b79d61
chlin <tainvecs@gmail.com>
2021-05-19 05:16:47 +0800

    Version Bump: 0.0.1

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject sentence-splitter "0.0.1-SNAPSHOT"
+(defproject org.clojars.chlin/sentence-splitter "0.0.1"
   :description "A simple sentence splitter based on Regex"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tainvecs/sentence-splitter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot sentence-splitter.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                              :creds :gpg}]])
